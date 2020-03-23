01a8e932947bbaf82a8291fc3e263ec61a538c7a
BPotet <bastien.potet@heig-vd.ch>
2020-03-23 11:10:22 +0100

    #1 - Adding the filters

--- Project/src/main/java/ch/heigvd/easytoolz/controllers/UserController.java
+++ Project/src/main/java/ch/heigvd/easytoolz/controllers/UserController.java
@@ -12,6 +12,8 @@ import java.util.Map;
 // TODO : One day remove this comment
 // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#reference
 // https://spring.io/guides/tutorials/rest/
+// !! Pour la recherche
+// https://blog.tratif.com/2017/11/23/effective-restful-search-api-in-spring/
 
 @RestController
 @RequestMapping ("/users")
@@ -20,14 +22,54 @@ public class UserController {
     UserRepository userRepository;
 
     @GetMapping
-    public List<User> index(){
-        return userRepository.findAll();
+    public List<User> index(
+            @RequestParam(value="firstName", required = false) String firstName,
+            @RequestParam(value = "lastName", required = false) String lastName,
+            @RequestParam(value = "userName", required = false) String userName,
+            @RequestParam(value = "email", required = false) String email
+    ){
+        firstName = transformLike(firstName);
+        lastName = transformLike(lastName);
+        userName = transformLike(userName);
+        email = transformLike(email);
+        if(firstName != null){
+            if(lastName != null){
+                return userRepository.findByFirstNameLikeAndLastNameLike(firstName, lastName);
+            }else{
+                return userRepository.findByFirstNameLike(firstName);
+            }
+        }else{
+            if(lastName != null){
+                return userRepository.findByLastNameLike(lastName);
+            }else if(userName != null){
+                return userRepository.findByUserNameLike(userName);
+            }else if(email != null){
+                return userRepository.findByEmailLike(email);
+            }
+            else{
+                return userRepository.findAll();
+            }
+        }
+    }
+
+    /**
+     * transform any string in LIKE string for the query
+     * for example :
+     * s => 'henri'
+     * return => '%henri%'
+     * @param s a string
+     * @return the string updated or null if s == null
+     */
+    private String transformLike(String s){
+        if(s == null)
+            return null;
+        return "%" + s + "%";
     }
 
     @GetMapping("/{username}")
     public User show(@PathVariable String username){
         return userRepository
-                .findById(username)
+                .findById(username + "a")
                 .orElseThrow(() -> new UserNotFoundException(username));
     }
 
@@ -38,6 +80,7 @@ public class UserController {
                     if(newUser.getFirstName() != null) oldUser.setFirstName(newUser.getFirstName());
                     if(newUser.getLastName() != null) oldUser.setLastName(newUser.getLastName());
                     if(oldUser.isAdmin() != newUser.isAdmin()) oldUser.setAdmin(newUser.isAdmin());
+                    if(newUser.getEmail() != null) oldUser.setEmail(newUser.getEmail());
                     return userRepository.save(oldUser);
                 })
                 .orElseThrow(
@@ -54,16 +97,12 @@ public class UserController {
         });
     }
 
-    @GetMapping("/search")
-    public List<User> search(@RequestBody Map<String, String> body){
-        String firstName = body.get("firstName");
-        String lastName = body.get("lastname");
-
-        return userRepository.findByFirstNameAndLastName(firstName, lastName);
-    }
-
     @PostMapping
     public User store(@RequestBody User user){
-        return userRepository.save(user);
+        if(userRepository.findById(user.getUserName()).isPresent()){
+            return null;
+        }else{
+            return userRepository.save(user);
+        }
     }
 }
--- Project/src/main/java/ch/heigvd/easytoolz/models/User.java
+++ Project/src/main/java/ch/heigvd/easytoolz/models/User.java
@@ -19,6 +19,9 @@ public class User {
     @Column(name="password")
     @NotNull
     private String password;
+    @Column(name="email")
+    @NotNull
+    private String email;
     @Column(name="isadmin")
     @NotNull
     private boolean isAdmin;
@@ -26,13 +29,14 @@ public class User {
     // Required for creating JSON parsing
     public User(){}
 
-    public User(String userName, String firstName, String lastName, String password, boolean isAdmin){
+    public User(String userName, String firstName, String lastName, String password, String email, boolean isAdmin){
         this.userName = userName;
         this.firstName = firstName;
         this.lastName = lastName;
         // TODO : Hasher le mot de passe
         this.password = password;
         this.isAdmin = isAdmin;
+        this.email = email;
     }
 
     public String getUserName() {
@@ -67,18 +71,11 @@ public class User {
         isAdmin = admin;
     }
 
-    @Override
-    public String toString() {
-        return String.format(
-                "User{" +
-                    "username=%s," +
-                    "firstName=%s," +
-                    "lastName=%s," +
-                    "isAdmin=%b" +
-                "}",
-                userName,
-                firstName,
-                lastName,
-                isAdmin);
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }

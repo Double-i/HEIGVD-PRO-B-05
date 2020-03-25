cc04958e9427844413babfd7289d091fc5bd7a28
Ilias <igoujgali@gmail.com>
2020-03-25 22:41:46 +0100

    Almost finished, still real connection to do #10
    Todo : real connection and user session handling

--- Project/src/main/frontend/src/signIn/SignIn.js
+++ Project/src/main/frontend/src/signIn/SignIn.js
@@ -1,20 +1,59 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Form, Button, Spinner, Modal } from 'react-bootstrap'
+import { Container, Form, Button, Spinner, Modal, Alert } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 
 function SignInForm(props) {
-    const schema = yup.object({
-        userEmail: yup.string().required(),
-        userPassword: yup.string().required(),
-    })
     const [showSigInForm, setShowSigInForm] = useState(false)
-    let values = {}
+    const [isLogging, setIsLogging] = useState(false)
+    const [hasConnectionProblem, setHasConnectionProblem] = useState(false)
+    const [hasBeenLoggedIn, setHasBeenLoggedIn] = useState(false) 
+    const [hasWrongCredential, sethasWrongCredential] = useState(false) 
+
+    const attemptLogin = (email, password) => {
+        console.log('attempt login...')
+        console.log('email: ', email)
+        console.log('password: ', password)
+
+
+        fetch('https://api.example.com/items')
+            .then(res => res.json())
+            .then(
+                result => {
+                    console.log('Connection ok')
+                    setIsLogging(false)
+                    setHasBeenLoggedIn(true)
+                    // TODO gestion session utilisateur
+                },
+
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                error => {
+                    // TODO check type d'erreur voir si errreur pour atteindre ou mauvais login
+
+                    console.log('Connection PAS ok')
+                    setHasConnectionProblem(true)
+                    sethasWrongCredential(true)
+                    setIsLogging(false)
+                }
+            )
+    }
+
+    // validation rules
+    const schema = yup.object({
+        userEmail: yup
+            .string()
+            .required('Requis')
+            .email(),
+        userPassword: yup.string().required('Requis'),
+    })
+
     return (
         <>
-            <Button onClick={() => setShowSigInForm(true)}>Se Loguer</Button>
+            <Button onClick={() => setShowSigInForm(true)}>Se Loguers</Button>
             <Modal
                 size="lg"
                 show={showSigInForm}
@@ -27,17 +66,33 @@ function SignInForm(props) {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    <Alert variant="success" hidden={!hasBeenLoggedIn}>
+                        Vous êtes connecté !
+                    </Alert>
+                    <Alert variant="danger" hidden={!hasConnectionProblem}>
+                        hum humm.. problème de connexion au serveur 
+                    </Alert>
+                    <Alert variant="warning" hidden={!hasWrongCredential}>
+                        Utilisateur inexistant
+                    </Alert>
                     <Container>
                         <Formik
                             validationSchema={schema}
-                            onSubmit={console.log}
-                            initialValues={{}}
+                            onSubmit={(values, { setSubmitting }) => {
+                                setIsLogging(!isLogging)
+                                attemptLogin(
+                                    values.userEmail,
+                                    values.userPassword
+                                )
+                            }}
+                            initialValues={{ userEmail: '', userPassword: '' }}
                         >
                             {({
                                 handleSubmit,
                                 handleChange,
                                 touched,
                                 isValid,
+                                values,
                                 errors,
                             }) => (
                                 <Form noValidate onSubmit={handleSubmit}>
@@ -81,14 +136,16 @@ function SignInForm(props) {
                                         type="submit"
                                         size="lg"
                                         block
+                                        disabled={isLogging}
                                     >
-                                        Se connecter
+                                        Se Connecter
                                         <Spinner
                                             as="span"
                                             animation="border"
                                             size="sm"
                                             role="status"
                                             aria-hidden="true"
+                                            hidden={!isLogging}
                                         />
                                     </Button>
                                 </Form>

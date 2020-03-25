5e91aa2ad151bffc84bbe6b448e8aa6f305c4866
Ilias <igoujgali@gmail.com>
2020-03-25 23:50:15 +0100

    Begin signup form still has to check address with google and post to api

--- Project/src/main/frontend/src/App.js
+++ Project/src/main/frontend/src/App.js
@@ -1,69 +1,67 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import {
-  BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-import { Button, Card } from 'react-bootstrap';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import  SignUpForm from "./signUp/SignUp";
 
 function App() {
   return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/"><Button>Home</Button></Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                <Button>Home</Button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/signup">Inscription</Link>
+            </li>
+          </ul>
+        </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
+        {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/signup">
+            <SignUpForm />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 //test comment for push
 function Home() {
-  return (<Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="TBD" />
-    <Card.Body>
-      <Card.Title>Home</Card.Title>
-      <Card.Text>
-        Some quick example text to build on the card title and make up the bulk of
-        the card's content.
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-  </Card>);
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="TBD" />
+      <Card.Body>
+        <Card.Title>Home</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
 function About() {
   return <h2>About</h2>;
 }
 
-function Users() {
-  return <h2>Users</h2>;
-}
+
 export default App;

e4d5f15d4452ee18643ef0bcccf15a74b8c38776
mauricelehmann <m.lehmann94@gmail.com>
2020-04-04 15:08:34 +0200

    Added some logic (if the user is logged or not) in the navbar

--- Project/src/main/frontend/src/userDashboard/DashBoard.js
+++ Project/src/main/frontend/src/userDashboard/DashBoard.js
@@ -1,10 +1,11 @@
 import * as React from "react";
+import { Link } from 'react-router-dom'
 
 class DashBoard extends React.Component {
     render() {
         return (
             <div>
-                C'est le DashBoard : contient le "addTools, editProfil, editToolz..."
+                <button><Link to="/AddTools">Add Tools</Link></button>
             </div>
         );
     }

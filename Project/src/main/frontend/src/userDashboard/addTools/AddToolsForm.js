30d352d984cd7fe8d33d4215095ff0886b4b6b01
Ilias <igoujgali@gmail.com>
2020-04-04 16:20:23 +0200

    fix onChange to update text in input

--- Project/src/main/frontend/src/userDashboard/addTools/AddToolsForm.js
+++ Project/src/main/frontend/src/userDashboard/addTools/AddToolsForm.js
@@ -1,62 +1,71 @@
-import * as React from "react";
-import * as yup from "yup";
-import regex from "../../common/regex";
-import {Formik} from "formik";
-import {Button, Form} from "react-bootstrap";
+import * as React from 'react'
+import * as yup from 'yup'
+import regex from '../../common/regex'
+import {Formik} from 'formik'
+import {Button, Form} from "react-bootstrap"
 
 function AddToolsForm(props) {
-
     //TODO : Move la validation d'une image ailleurs?
-    const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
-    const FILE_SIZE = 10000;
+    const SUPPORTED_FORMATS = [
+        'image/jpg',
+        'image/jpeg',
+        'image/gif',
+        'image/png',
+    ]
+    const FILE_SIZE = 10000
     const validateImageType = (value) => {
-        if(value) {
+        if (value) {
             let type = value.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0]
             return SUPPORTED_FORMATS.includes(type)
         }
     }
 
-    const attemptAddTool = values => {
+    const attemptAddTool = (values) => {
         //TODO
-        alert(values);
-    };
+        alert(values)
+    }
 
+
+    // Ilias: la validation pour l'image ne semble pas correcte il doit avoir une petite erreur ...à voir
 
     // validation rules
     const schema = yup.object({
-        toolName: yup
-            .string()
-            .required('Requis'),
-        toolDescription: yup
-            .string()
-            .min(0)
-            .max(200),
-        toolImage: yup.array()
+        toolName: yup.string().required('Requis'),
+        toolDescription: yup.string().min(0).max(200),
+        toolImage: yup
+            .array()
             .nullable()
             .required('Validation requise')
-            .test('fileSize', "File is too large", value => value.size <= FILE_SIZE) .test('fileType', "Your Error Message", value => SUPPORTED_FORMATS.includes(value.type) ),
-    });
+            .test(
+                'fileSize',
+                'File is too large',
+                (value) =>
+                    typeof value !== 'undefined' && value.size <= FILE_SIZE
+            )
+            .test(
+                'fileType',
+                'Your Error Message',
+                (value) =>
+                    typeof value !== 'undefined' &&
+                    SUPPORTED_FORMATS.includes(value.type)
+            ),
+    })
 
-    /**
-     * Maurice : Pourquoi on ne peut rien écrire dans les fields ??? Est-ce que c'est à cause de onChange={handleChange} ?!
-     */
     return (
         <>
             <Formik
                 validationSchema={schema}
-                onSubmit={(values, { setSubmitting }) => {
-                    attemptAddTool(values);
-                    alert("yooy");
+                onSubmit={(values, {setSubmitting}) => {
+                    attemptAddTool(values)
+                    alert('yooy')
                 }}
                 initialValues={{
                     toolName: '',
                     toolDescription: '',
-                    toolImage: ''
+                    toolImage: '',
                 }}
             >
-                {({
-                  values, handleSubmit, handleChange
-                }) => (
+                {({values, handleSubmit, handleChange}) => (
                     <Form noValidate onSubmit={handleSubmit}>
                         <Form.Group controlId="fromToolName">
                             <Form.Control
@@ -64,36 +73,35 @@ function AddToolsForm(props) {
                                 type="text"
                                 placeholder="Nom de l'outil"
                                 value={values.toolName}
-                                onchange={handleChange}
+                                onChange={handleChange}
                             />
                         </Form.Group>
                         <Form.Group controlId="fromToolDescription">
                             <Form.Control
                                 name="toolDescription"
                                 type="text"
-                                placeholder= "Description de l'outil"
+                                placeholder="Description de l'outil"
+                                onChange={handleChange}
                                 value={values.toolDescription}
                             />
                         </Form.Group>
                         <Form.Group controlId="fromToolFile">
                             <Form.Control
-                                name="toolFile"
+                                name="toolImage"
                                 type="file"
-                                placeholder= "Photo de l'outil"
+                                placeholder="Photo de l'outil"
+                                onChange={handleChange}
                                 value={values.toolImage}
                             />
                         </Form.Group>
-                        <Button
-                            variant="primary"
-                            type="submit"
-                            size="lg"
-                            block
-                        >Valider</Button>
+                        <Button variant="primary" type="submit" size="lg" block>
+                            Valider
+                        </Button>
                     </Form>
-            )}
+                )}
             </Formik>
         </>
     )
 }
 
-export default AddToolsForm;
+export default AddToolsForm

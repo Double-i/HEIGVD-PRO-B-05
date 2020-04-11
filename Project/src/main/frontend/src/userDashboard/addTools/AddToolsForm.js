3e2676f0ea955e37b1956553d7923ae500fb6715
Ilias <igoujgali@gmail.com>
2020-04-11 17:24:25 +0200

    fixed image problem

--- Project/src/main/frontend/src/userDashboard/addTools/AddToolsForm.js
+++ Project/src/main/frontend/src/userDashboard/addTools/AddToolsForm.js
@@ -1,145 +1,185 @@
 import * as React from 'react'
 import * as yup from 'yup'
-import regex from '../../common/regex'
-import {Formik} from 'formik'
-import {Alert, Button, Form} from "react-bootstrap"
-import {sendRequest} from "../../common/ApiHelper";
-import {schema} from "json-schema-traverse";
+import { Formik } from 'formik'
+import { Alert, Button, Form } from 'react-bootstrap'
+import { sendRequest, sendEzApiRequest } from '../../common/ApiHelper'
+import { schema } from 'json-schema-traverse'
 
-class AddToolsForm extends React.Component{
-
+class AddToolsForm extends React.Component {
     //TODO : Get tag from Database!
-    tags = [
-        'jardin',
-        'bois',
-        'éléctronique'
-    ];
+    tags = ['jardin', 'bois', 'éléctronique']
 
-    // TODO delete comment - Pour Bastien, le /api est ajouté automatiquement, change signUpAPIEndpoint avec le bon endpoint
-    addToolAPIEndpoint = '/tools';
+    addToolAPIEndpoint = '/tools'
 
     //TODO : Move la validation d'une image ailleurs?
-    SUPPORTED_FORMATS = [
-        'image/jpg',
-        'image/jpeg',
-        'image/gif',
-        'image/png',
-    ];
-    FILE_SIZE = 1000000;
-
+    SUPPORTED_FORMATS = ['image/jpg', 'image/jpeg', 'image/gif', 'image/png']
+    FILE_SIZE = 1000000
 
     attemptAddTool = (values) => {
+        console.log('attemptAddTool', values)
+        const data = {
+            name: values.toolName,
+        }
 
-        sendRequest(this.addToolAPIEndpoint,{
-          data : JSON.stringify(values) //TODO : problème avec le fetch encore , a regarder commrnt on fait
-        }).then(
-                result => {
-                    console.log("Ajout de l'outil ok!")
-                }
-            )
-    };
+        // A voir en fonction de notre API
+        sendEzApiRequest('/tools', 'POST', data).then(
+            (result) => {
+                console.log(result)
+            },
+            (errors) => {
+                console.log(errors)
+            }
+        )
+    }
 
-    // Ilias: la validation pour l'image ne semble pas correcte il doit avoir une petite erreur ...à voir
+    //TODO  Ilias : < !!! > ATTENTION, en JS le ``this`` est OBLIGATOIRE. Ex:  validationSchema={schema} utilise une variable schema qui est probablement undefined meme si this.schema est défini
 
     // validation rules
     schema = yup.object({
-        toolName: yup.string().required('Requis').min(0).max(20),
+        toolName: yup
+            .string()
+            .required('Requis')
+            .min(3, 'UNE MESSAGE ICI AUTREMENT ANGLAIS')
+            .max(20),
         toolDescription: yup.string().min(0).max(200),
         toolTags: yup.array().of(yup.string()).min(1),
         toolImage: yup
             .mixed()
             //TODO : Les tests ne marche pas, peut être doit on faire un autre schema pour que les files ?
-/*            .test(
+            .test(
                 'fileSize',
                 'File is too large',
                 (value) =>
-                     typeof value !== 'undefined' && value.size <= FILE_SIZE
+                    typeof value !== 'undefined' && value.size <= this.FILE_SIZE
             )
-           .test(
+            .test(
                 'fileType',
                 'Your Error Message',
                 (value) =>
                     typeof value !== 'undefined' &&
-                    SUPPORTED_FORMATS.includes(value.type)
-            )*/
-    });
+                    this.SUPPORTED_FORMATS.includes(value.type)
+            ),
+    })
     render() {
+        return (
+            <>
+                <h1>Ajouter un outil</h1>
+                <Formik
+                    validationSchema={this.schema}
+                    onSubmit={(values, { setSubmitting }) => {
+                        this.attemptAddTool(values)
+                    }}
+                    initialValues={{
+                        toolName: '',
+                        toolDescription: '',
+                        toolImage: '',
+                        toolTags: [],
+                    }}
+                >
+                    {({
+                        values,
+                        handleSubmit,
+                        handleChange,
+                        handleBlur,
+                        tags,
+                        touched,
+                        isValid,
+                        isInvalid,
+                        errors,
+                        setFieldValue,
+                    }) => (
+                        <Form noValidate onSubmit={handleSubmit}>
+                            {/* ------------------------------------------------------------------------------------- */}
+                            {/* TODO: à reproduire sur les autres champs () */}
+                            {/* https://react-bootstrap.netlify.com/components/forms/#forms-validation-libraries */}
 
-    return (
-        <>
-            <h1>Ajouter un outil</h1>
-            <Formik
-                validationSchema={schema}
-                onSubmit={(values, {setSubmitting}) => {
-                    this.attemptAddTool(values);
-                }}
-                initialValues={{
-                    toolName: '',
-                    toolDescription: '',
-                    toolImage: '',
-                    toolTags: '',
-                }}
-            >
-                {({values, handleSubmit, handleChange, tags}) => (
+                            <Form.Group controlId="formToolName">
+                                {JSON.stringify(values)}
+                                {JSON.stringify(tags)}
+                                <Form.Control
+                                    name="toolName"
+                                    type="text"
+                                    placeholder="Nom de l'outil"
+                                    value={values.toolName}
+                                    onChange={handleChange}
 
-                    <Form noValidate onSubmit={handleSubmit}>
-                        <Form.Group controlId="formToolName">
-                            {JSON.stringify(values)}
-                            {JSON.stringify(tags)}
-                            <Form.Control
-                                name="toolName"
-                                type="text"
-                                placeholder="Nom de l'outil"
-                                value={values.toolName}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Group controlId="formToolDescription">
-                            <Form.Control
-                                name="toolDescription"
-                                type="text"
-                                placeholder="Description de l'outil"
-                                onChange={handleChange}
-                                value={values.toolDescription}
-                            />
-                        </Form.Group>
-                        <Form.Group controlId={"formToolTags"}>
-                            Tags (ctrl + click pour choisir plusieurs tags)
-                            <select
-                                multiple="multiple"
-                                className="form-control"
-                                name="toolTags"
-                                value={values.toolTags}
-                                onChange={handleChange}
-                                style={{ display: 'block' }}
-                            >
-                                {(() => {
-                                    const options = [];
-                                    for (let i = 0; i < this.tags.length; i++) {
-                                        options.push(<option value={this.tags[i]}>{this.tags[i]}</option>);
+
+                                    onBlur={handleBlur} // TODO OBLIGATOIRE POUR UTILISER TOUCHED https://stackoverflow.com/questions/57385931/why-isnt-the-formik-touched-property-being-populated
+                                    isInvalid={ 
+                                        touched.toolName && !!errors.toolName
                                     }
-                                    return options;
-                                })()}
+                                    isValid={
+                                        touched.toolName && !errors.toolName
+                                    }  
+                                />
 
-                            </select>
-                        </Form.Group>
-                        <Form.Group controlId="formToolFile">
-                            <Form.Control
-                                name="toolImage"
-                                type="file"
-                                placeholder="Photo de l'outil"
-                                onChange={handleChange}
-                                value={values.toolImage}
-                            />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" size="lg" block>
-                            Valider
-                        </Button>
-                    </Form>
-                )}
-            </Formik>
-        </>
-    )}
+                                {/* TODO  <!> les feedback DOIVENT être EN DESSOUS du <FORM.CONTROL>  */}
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.toolName}
+                                </Form.Control.Feedback>
+                                <Form.Control.Feedback>
+                                    OK !
+                                </Form.Control.Feedback>
+
+                            </Form.Group>
+                            {/* ############################################################################################ */}
+
+                            <Form.Group controlId="formToolDescription">
+                                <Form.Control
+                                    name="toolDescription"
+                                    type="text"
+                                    placeholder="Description de l'outil"
+                                    onChange={handleChange}
+                                    value={values.toolDescription}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId={'formToolTags'}>
+                                Tags (ctrl + click pour choisir plusieurs tags)
+                                <select
+                                    multiple="multiple"
+                                    className="form-control"
+                                    name="toolTags"
+                                    value={values.toolTags}
+                                    onChange={handleChange}
+                                    style={{ display: 'block' }}
+                                >
+                                    {this.tags.map((value, idx) => (
+                                        <option
+                                            key={`tag-${idx}`}
+                                            value={value}
+                                        >
+                                            {value}
+                                        </option>
+                                    ))}
+                                </select>
+                            </Form.Group>
+                            <Form.Group controlId="formToolFile">
+                                <Form.Control
+                                    name="toolImage"
+                                    type="file"
+                                    placeholder="Photo de l'outil"
+                                    onChange={(event) => {
+                                        setFieldValue(
+                                            'toolImage',
+                                            event.currentTarget.files[0]
+                                        )
+                                    }}
+                                />
+                            </Form.Group>
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                size="lg"
+                                block
+                            >
+                                Valider
+                            </Button>
+                        </Form>
+                    )}
+                </Formik>
+            </>
+        )
+    }
 }
 
 export default AddToolsForm

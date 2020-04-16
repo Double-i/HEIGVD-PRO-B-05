9aac097cc8bad147bc6486b79e4952ba1cfa9d60
BPotet <bastien.potet@heig-vd.ch>
2020-04-16 14:42:28 +0200

    #21 Adding TagService and TagController

--- Project/src/main/java/ch/heigvd/easytoolz/repositories/TagRepository.java
+++ Project/src/main/java/ch/heigvd/easytoolz/repositories/TagRepository.java
@@ -1,10 +1,10 @@
 package ch.heigvd.easytoolz.repositories;
 
-import ch.heigvd.easytoolz.models.EZObject;
 import ch.heigvd.easytoolz.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag,String> {
+    List<Tag> findByNameLike(String name);
 }

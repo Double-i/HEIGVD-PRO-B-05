b6becb13906cf12f9bdbed675ab466ca97ed2697
BPotet <bastien.potet@heig-vd.ch>
2020-04-29 10:21:52 +0200

    Fix a little bug

--- Project/src/main/java/ch/heigvd/easytoolz/specifications/UserSpecs.java
+++ Project/src/main/java/ch/heigvd/easytoolz/specifications/UserSpecs.java
@@ -1,6 +1,7 @@
 package ch.heigvd.easytoolz.specifications;
 
 import ch.heigvd.easytoolz.models.User;
+import ch.heigvd.easytoolz.models.User_;
 import ch.heigvd.easytoolz.util.ServiceUtils;
 import org.springframework.data.jpa.domain.Specification;
 

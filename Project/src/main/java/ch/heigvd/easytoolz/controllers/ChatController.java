fa3cbe9354ef76257e106cc265c72e041f020d21
bpotet <bastien.potet@heig-vd.ch>
2020-05-13 11:37:36 +0200

    WIP on #47

--- Project/src/main/java/ch/heigvd/easytoolz/controllers/ChatController.java
+++ Project/src/main/java/ch/heigvd/easytoolz/controllers/ChatController.java
@@ -2,7 +2,12 @@ package ch.heigvd.easytoolz.controllers;
 
 import ch.heigvd.easytoolz.models.ChatMessage;
 import ch.heigvd.easytoolz.models.OutputMessage;
+import ch.heigvd.easytoolz.models.StateNotification;
+import ch.heigvd.easytoolz.models.User;
 import ch.heigvd.easytoolz.repositories.ChatRepository;
+import ch.heigvd.easytoolz.services.interfaces.NotificationService;
+import ch.heigvd.easytoolz.services.interfaces.UserService;
+import ch.heigvd.easytoolz.util.ServiceUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -25,13 +30,21 @@ public class ChatController {
     @Autowired
     SimpMessagingTemplate simpMessagingTemplate;
 
+    @Autowired
+    NotificationService notificationService;
+
+    @Autowired
+    UserService userService;
+
     @MessageMapping("/EZChat/{loan-conversation}/private")
     @SendTo("/secured/user/queue/loan-room/{loan-conversation}")
     public ChatMessage sendMessage(@Payload ChatMessage message,SimpMessageHeaderAccessor sha , @DestinationVariable("loan-conversation") String user)
     {
-
         repository.save(message);
-        System.out.println("FROM ; "+sha.getUser().getName()+" TO "+ user + " SESSION " + sha.getSessionId());
+
+        User recipient = userService.getUser(user);
+
+        notificationService.storeNotification(ServiceUtils.createNotification(StateNotification.MESSAGE, recipient, message.getSender()));
 
         return message;
 

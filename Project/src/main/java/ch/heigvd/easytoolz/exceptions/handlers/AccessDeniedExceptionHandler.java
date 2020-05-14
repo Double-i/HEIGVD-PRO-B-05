6e068c78146b4be81a197aa126159dcd3c917279
bpotet <bastien.potet@heig-vd.ch>
2020-05-14 10:05:42 +0200

    WIP on #52 / Little fix for BadCredentialsException

--- Project/src/main/java/ch/heigvd/easytoolz/exceptions/handlers/AccessDeniedExceptionHandler.java
+++ Project/src/main/java/ch/heigvd/easytoolz/exceptions/handlers/AccessDeniedExceptionHandler.java
@@ -5,15 +5,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+@ControllerAdvice
 public class AccessDeniedExceptionHandler extends DefaultExceptionHandler {
 
     @ExceptionHandler({AccessDeniedException.class})
-    public ResponseEntity<ApiError> handleEZObjectNotFoundException(AccessDeniedException ex, WebRequest request)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException ex, WebRequest request)
     {
         return makeError(ex,request,HttpStatus.FORBIDDEN);
+    }
 
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity<ApiError> handleBadCredentialsException(BadCredentialsException ex, WebRequest request)
+    {
+        return makeError(ex, request, HttpStatus.UNAUTHORIZED);
     }
 }
--- Project/src/main/java/ch/heigvd/easytoolz/exceptions/handlers/UserExceptionHandler.java
+++ Project/src/main/java/ch/heigvd/easytoolz/exceptions/handlers/UserExceptionHandler.java
@@ -8,9 +8,11 @@ import ch.heigvd.easytoolz.exceptions.user.UserNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+@ControllerAdvice
 public class UserExceptionHandler extends DefaultExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ApiError> handleUserNotFoundException(EZObjectNotFoundException ex, WebRequest request)

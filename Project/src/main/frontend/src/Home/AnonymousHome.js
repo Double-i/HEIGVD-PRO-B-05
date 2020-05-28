f35fcbd5719d74837b185f3f912110717b51ebdf
Robin Reuteler <44259320+reutelerr@users.noreply.github.com>
2020-05-28 16:36:39 +0200

    Correction de l'orthographe

--- Project/src/main/frontend/src/Home/AnonymousHome.js
+++ Project/src/main/frontend/src/Home/AnonymousHome.js
@@ -31,7 +31,7 @@ function AnonymousHome(props) {
                 <br/>
                 <p>
 
-                    Vous êtes l'heureux propriétaire d'une magnifique perceuse Bosch cadencé à 1000 rpm avec ses méches incassables
+                    Vous êtes l'heureux propriétaire d'une magnifique perceuse Bosch cadencée à 1000 rpm avec ses méches incassables
                     de qualité allemande. Cependant, vous vous sentez coupable de l'utiliser que 2 fois par année.
                 </p>
                 <p>Pas de soucis, <Link to={"/signup"}>inscrivez-vous</Link> tout de suite sur Easytools, une plateforme parfaite pour partager ses outils.</p>
@@ -43,8 +43,8 @@ function AnonymousHome(props) {
                 <br/>
                 <h3>Vous manquez d'outil</h3>
                 <br/>
-                <p>Après un passage chez Ikea, vous avez dépensé vos dernière économie pour la table en verre Fäêkjö
-                    mais vous vous rendez compte après coup que votre marteau n'est pas l'outil le plus adatpé pour monter votre meuble.
+                <p>Après un passage chez Ikea, vous avez dépensé vos dernières économies pour la table en verre Fäêkjö
+                    mais vous vous rendez compte après coup que votre marteau n'est pas l'outil le plus adapté pour monter votre meuble.
 
                 </p>
                 <p>

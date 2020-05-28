5cdf3eb2ead1383ebc68c1bcbefd4e8082c846d2
Ilias <igoujgali@gmail.com>
2020-05-28 10:05:01 +0200

    add comments

--- Project/src/main/java/ch/heigvd/easytoolz/models/dto/PeriodRequest.java
+++ Project/src/main/java/ch/heigvd/easytoolz/models/dto/PeriodRequest.java
@@ -4,6 +4,9 @@ import ch.heigvd.easytoolz.models.State;
 
 import java.util.Date;
 
+/**
+ * This class is used to implement the data send via a POST request to add or update a loan period
+ */
 public class PeriodRequest {
     private Date dateStart;
     private Date dateEnd;
